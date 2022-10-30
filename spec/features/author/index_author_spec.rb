b1f3f002150f3e5b216bcaac8fd5bc9583d05a48
Alexander Ungefug <82446024+AlexanderUngefug@users.noreply.github.com>
2022-10-30 12:09:20 +0100

    fix #8

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
  describe "Index author page", type: :feature do
    
-  it "Author index page should list name and homepage of all authors" do
-    
+  it "Author index page should list name and homepage of all authors" do    
     visit authors_path
     expect(page).to have_text('All')    
   end
+
+  it "Link to new author" do    
+    visit authors_path
+    expect(page).to have_link 'New', href: new_author_path 
+  end
+  
 
  end
