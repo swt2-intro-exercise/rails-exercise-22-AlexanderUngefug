069fff7eeddd4255b0528c09995f391e69aa0477
Alexander Ungefug <82446024+AlexanderUngefug@users.noreply.github.com>
2022-10-30 12:17:31 +0100

    fix #9

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,8 @@ class Author < ApplicationRecord
     def name
         first_name + " " + last_name
     end
+
+    validates :last_name, presence: true, 
+                          length: { minimum: 2 }, 
+                          format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
