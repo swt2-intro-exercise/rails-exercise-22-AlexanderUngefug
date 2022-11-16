17540dfb7c663d53792d6137dce31dcae561f459
Alexander Ungefug <82446024+AlexanderUngefug@users.noreply.github.com>
2022-11-16 11:42:49 +0100

    fix #21

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -5,4 +5,5 @@ class Paper < ApplicationRecord
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    
 end
